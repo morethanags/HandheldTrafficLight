625221a5226adb96e4189bdecf76b1bea906aef3
dmoran <dmoran@huntloc.pe>
2017-01-10 16:28:40 -0500

    Heights

--- app/src/main/java/com/huntloc/handheld/JournalFragment.java
+++ app/src/main/java/com/huntloc/handheld/JournalFragment.java
@@ -90,7 +90,9 @@ public class JournalFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_journal,
+
+
+        View view = inflater.inflate(R.layout.fragment_journal_cel,
                 container, false);
         textView_Credential = (TextView) view.findViewById(R.id.textView_Credential);
         textView_Name = (TextView) view.findViewById(R.id.textView_Name);
